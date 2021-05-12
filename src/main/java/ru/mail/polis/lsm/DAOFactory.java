277094d929e65197312efb892fd20eddb5918753
Vadim Tsesko <incubos@users.noreply.github.com>
2021-05-12 12:33:04 +0300

    Added private constructor to DAOFactory

--- src/main/java/ru/mail/polis/lsm/DAOFactory.java
+++ src/main/java/ru/mail/polis/lsm/DAOFactory.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 
 public class DAOFactory {
 
+    private DAOFactory() {
+        // Only static methods
+    }
+
     public static DAO create(DAOConfig config) throws IOException {
         throw new UnsupportedOperationException("Implement me");
     }
