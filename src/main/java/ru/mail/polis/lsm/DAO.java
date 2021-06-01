13c690637eaf4f368c12c93d711659a3a25de6cd
Stepan Ponomarev <step.ponomarev@gmail.com>
2021-06-01 12:43:54 +0300

    IDK

--- src/main/java/ru/mail/polis/lsm/DAO.java
+++ src/main/java/ru/mail/polis/lsm/DAO.java
@@ -32,36 +32,39 @@ public interface DAO extends Closeable {
         return result;
     }
 
-    static int ДЕРЖИМ_ВОТ_СТОЛЬКО_НАГРУЗКИ = 1000000;
+    int STRICT_SYSTEM_LIMIT = 1000000;
+
     /**
-     * Метод сливает итераторы в один, упорядочивая по возрастанию, 
+     * Метод сливает итераторы в один, упорядочивая по возрастанию,
      * если данные повторяются - берет последнюю версию данных.
-     * 
+     *
      * @param iterators список итераторов для слияния
      * @return последовательность итераторов
      */
     static Iterator<Record> merge(List<Iterator<Record>> iterators) {
         return iterators.stream()
                 .flatMap(DAO::toStream)
-                .limit(ДЕРЖИМ_ВОТ_СТОЛЬКО_НАГРУЗКИ)
+                .limit(STRICT_SYSTEM_LIMIT)
                 .collect(Collectors.groupingBy(Record::getKey))
                 .values()
                 .stream()
-                .reduce(new ArrayList<>(), 
-                        ((l, r) -> {
-                            var rList = r.stream()
-                                    .filter(rec -> Objects.equals(rec, r.get(r.size() - 1)))
-                                    .collect(Collectors.toList());
-                            
-                            l.addAll(rList);
-                            
-                            return l;
-                        }))
-                .stream().sorted(Comparator.comparing(Record::getKey)).iterator();
+                .reduce(new ArrayList<>(), DAO::recordReducer).stream()
+                .sorted(Comparator.comparing(Record::getKey))
+                .iterator();
     }
 
     private static Stream<Record> toStream(Iterator<Record> iterator) {
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, 0), false);
+    }
+
+    private static List<Record> recordReducer(final List<Record> acc, final List<Record> current) {
+        var lastValueRecordList = current.stream()
+                .filter(rec -> Objects.equals(rec, current.get(current.size() - 1)))
+                .collect(Collectors.toList());
+
+        acc.addAll(lastValueRecordList);
+
+        return acc;
     }
 
     Iterator<Record> range(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey);
