ee6ae4cc576eb79de1475b66f7401c1e6a4695ca
Stepan Ponomarev <step.ponomarev@gmail.com>
2021-07-03 14:33:40 +0300

    basic test works

--- src/main/java/ru/mail/polis/lsm/DAOFactory.java
+++ src/main/java/ru/mail/polis/lsm/DAOFactory.java
@@ -1,6 +1,6 @@
 package ru.mail.polis.lsm;
 
-import ru.mail.polis.lsm.ponomarev.PonomarevDAO;
+import ru.mail.polis.lsm.ponomarev.UltraSuperFastDAOImpl;
 
 import java.io.IOException;
 
@@ -14,6 +14,6 @@ public final class DAOFactory {
      * Create an instance of {@link DAO} with supplied {@link DAOConfig}.
      */
     public static DAO create(DAOConfig config) throws IOException {
-        return new PonomarevDAO(config);
+        return new UltraSuperFastDAOImpl(config);
     }
 }
---
+++ src/main/java/ru/mail/polis/lsm/ponomarev/UltraSuperFastDAOImpl.java
@@ -0,0 +1,63 @@
+package ru.mail.polis.lsm.ponomarev;
+
+import ru.mail.polis.lsm.DAO;
+import ru.mail.polis.lsm.DAOConfig;
+import ru.mail.polis.lsm.Record;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.NavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class UltraSuperFastDAOImpl implements DAO {
+    private static final int MEMORY_LIMIT = 123456;
+
+    private final NavigableMap<ByteBuffer, Record> store;
+    
+    /**
+     * @param config конфигурация дао
+     * @throws IOException выбрасывает в случае чего вдруг, такое возможно
+     */
+    public UltraSuperFastDAOImpl(DAOConfig config) throws IOException {
+        this.store = new ConcurrentSkipListMap<>();
+    }
+
+    @Override
+    public Iterator<Record> range(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
+        if (fromKey == null && toKey == null) {
+            return this.store.values().stream().filter(r -> !r.isTombstone()).iterator();
+        }
+        
+        return this.store.values()
+                .stream()
+                .filter(r -> recordFilter(r, fromKey, toKey))
+                .iterator();
+    }
+
+    private boolean recordFilter(Record record, ByteBuffer fromKey, ByteBuffer toKey) {
+        if (record.isTombstone()) {
+            return false;
+        }
+
+        return fromKey == null
+                ? record.getKey().compareTo(toKey) <= 0
+                : record.getKey().compareTo(fromKey) >= 0;
+    }
+    
+    @Override
+    public void upsert(Record record) {
+        ByteBuffer key = record.getKey();
+        ByteBuffer value = record.getValue();
+
+        Record newRecord = value == null ? Record.tombstone(key) : Record.of(key, value);
+        
+        this.store.put(key, newRecord);
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.store.clear();
+    }
+}
